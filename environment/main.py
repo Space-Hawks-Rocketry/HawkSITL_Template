d6c6e1de0de0e33ef7ffa586397871d1ad9fc18a
Tesseraction <timburnstech@gmail.com>
2026-05-27 05:18:38 +0000

    terrible drone demo

--- environment/main.py
+++ environment/main.py
@@ -1,86 +1,146 @@
 from time import time, sleep
 import numpy as np
 import json
+import matplotlib.pyplot as plt
 
 from framework.IPC.IPC_computer import IPC_Computer
 
-class Square:
+# class Square:
   
-  def __init__(self, m=1.0, I=1.0, pos=np.zeros(2, dtype=float), vel=np.zeros(2, dtype=float), radians=0.0, radians_rate=0.0):
-    self.m = m
-    self.I = I
+#   def __init__(self, m=1.0, I=1.0, pos=np.zeros(2, dtype=float), vel=np.zeros(2, dtype=float), radians=0.0, radians_rate=0.0):
+#     self.m = m
+#     self.I = I
+#     self.pos = pos
+#     self.vel = vel
+#     self.radians = radians
+#     self.radians_rate = radians_rate
+    
+#     self.force = np.zeros(2, dtype=float)
+#     self.torque = 0.0
+    
+#   def getDCMtoBody(self):
+#     return np.array([
+#       [np.cos(self.radians), np.sin(self.radians)],
+#       [-np.sin(self.radians), np.cos(self.radians)]
+#     ])
+  
+#   def applyForce(self, force):
+#     self.force += force
+    
+#   def applyTorque(self, torque):
+#     self.torque += torque
+  
+#   def update(self, dt):
+#     self.vel += dt * self.force / self.m
+#     self.pos += dt * self.vel
+    
+#     self.radians_rate += dt * self.torque / self.I
+#     self.radians += dt * self.radians_rate
+    
+#     self.force = np.zeros(2, dtype=float)
+#     self.torque = 0.0
+    
+
+class Drone:
+  
+  def __init__(self, pos=np.zeros(2, dtype=float), vel=np.zeros(2, dtype=float), radians=0.0, radians_rate=0.0, m=1.0, I=1.0, width=2.0):
     self.pos = pos
     self.vel = vel
     self.radians = radians
     self.radians_rate = radians_rate
-    
-    self.force = np.zeros(2, dtype=float)
-    self.torque = 0.0
+    self.m = m
+    self.I = I
+    self.width = width
+    self.fan1_thrust = 0
+    self.fan2_thrust = 0
     
   def getDCMtoBody(self):
     return np.array([
       [np.cos(self.radians), np.sin(self.radians)],
       [-np.sin(self.radians), np.cos(self.radians)]
     ])
-  
-  def applyForce(self, force):
-    self.force += force
     
-  def applyTorque(self, torque):
-    self.torque += torque
-  
+  def setFanThrust(self, fan1: float, fan2: float):
+    self.fan1_thrust = max(0, fan1)
+    self.fan2_thrust = max(0, fan2)
+    
   def update(self, dt):
-    self.vel += dt * self.force / self.m
+    force = self.getDCMtoBody().T @ np.array([0, 2 * min(self.fan1_thrust, self.fan2_thrust)]) + np.array([0, -9.8*self.m])
+    torque = 0.5 * self.width * (self.fan2_thrust - self.fan1_thrust)
+    
+    self.vel += dt * force / self.m
     self.pos += dt * self.vel
     
-    self.radians_rate += dt * self.torque / self.I
+    self.radians_rate += dt * torque / self.I
     self.radians += dt * self.radians_rate
-    
-    self.force = np.zeros(2, dtype=float)
-    self.torque = 0.0
-    
     
 
 ipc = IPC_Computer()
-sleep(2)
 ipc.start(3563)
 
 
-square = Square(pos=np.array([0, 0], dtype=float))
-target_pos = np.array([90.0, 45.0])
+# square = Square(pos=np.array([0, 0], dtype=float))
+drone = Drone()
+target_pos = np.array([0.1, 5.0])
 
-dt = 0
+
+dt = 0.01
 t = 0
+sim_time = 10
 running = True
+
+x_data = []
+y_data = []
 
 ipc.sendJSON({
   "posx": 0.0,
   "posy": 0.0,
   "velx": 0.0,
-  "vely": 0.0
+  "vely": 0.0,
+  "radians": 0.0,
+  "radians_rate": 0.0
 })
 res_json = ipc.recvJSON()
-res_force = np.array([res_json["Fx"], res_json["Fy"]])
+# res_force = np.array([res_json["F1"], res_json["F2"]])
 
 while running:
   start_time = time()
   
   ipc.sendJSON({
-    "posx": target_pos[0] - square.pos[0],
-    "posy": target_pos[1] - square.pos[1],
-    "velx": square.vel[0],
-    "vely": square.vel[1]
+    "posx": target_pos[0] - drone.pos[0],
+    "posy": target_pos[1] - drone.pos[1],
+    "velx": 0 - drone.vel[0],
+    "vely": 0 - drone.vel[1],
+    "radians": drone.radians,
+    "radians_rate": drone.radians_rate
   })
   res_json = ipc.recvJSON()
-  res_force = np.array([res_json["Fx"], res_json["Fy"]])
-  print(f"(PHYSICS): Received control maneuver @ t={round(t,3)}s")
+  commands = res_json["commands"]
+  if ("F1" in commands) and ("F2" in commands):
+    drone.setFanThrust(commands["F1"], commands["F2"])
   
-  square.applyForce(np.array([
-    res_force[0],
-    res_force[1]
-  ]))
-  square.update(dt)
+  # print(commands)
+  # res_force = np.array([res_json["Fx"], res_json["Fy"]])
+  # print(f"(PHYSICS): Received control maneuver @ t={round(t,3)}s")
   
-  dt = time() - start_time
+  # square.applyForce(np.array([
+  #   res_force[0],
+  #   res_force[1]
+  # ]))
+  # square.update(dt)
+  # drone.setFanThrust()
+  
+  drone.update(dt)
+  
+  x_data.append(drone.pos[0])
+  y_data.append(drone.pos[1])
+  
+  if t >= sim_time:
+    break
+  
+  # dt = time() - start_time
   t += dt
-  
+
+plt.plot(x_data, y_data)
+plt.plot(target_pos[0], target_pos[1], "ro")
+plt.savefig("myfig.png")
--- flight-computer/src/main.cpp
+++ flight-computer/src/main.cpp
@@ -1,77 +1,13 @@
-// #include <zmq.hpp>
-// #include <nlohmann/json.hpp>
-// #include <iostream>
-// #include <string.h>
-// using json = nlohmann::json;
-
-// int main()
-// {
-    // zmq::context_t context;
-    // zmq::socket_t socket(context, zmq::socket_type::rep);
-//     // socket.bind("inproc://test");
-    
-//     socket.bind("tcp://localhost:3563");
-    
-//     zmq::message_t request;
-
-//     while (true) {
-        
-//         auto result = socket.recv(&request);
-
-//         if (result) {
-//             // Convert the raw ZMQ message into a std::string
-//             std::string json_string(static_cast<char*>(request.data()), request.size());
-//             std::cout << "\n(COMPUTER): Received sensor data -> " << json_string << std::endl;
-
-//             try {
-//                 // Parse the string into a JSON object
-//                 json data = json::parse(json_string);
-                
-                // // Extract data safely using native C++ types
-                // float posx = data["posx"].get<float>();
-                // float posy = data["posy"].get<float>();
-                // float velx = data["velx"].get<float>();
-                // float vely = data["vely"].get<float>();
-
-                // float k1 = 3.0;
-                // float k2 = -3.0;
-
-                // json response = {
-                //     {"Fx", k1 * posx + k2 * velx},
-                //     {"Fy", k1 * posy + k2 * vely},
-                // };
-
-//                 std::string response_str = response.dump();
-//                 // // TODO: Send JSON String Back!!
-//                 socket.send(zmq::buffer(response_str), zmq::send_flags::none);
-
-//             } catch (const json::parse_error& e) {
-//                 std::cerr << "JSON Parsing error: " << e.what() << std::endl;
-//                 socket.send(zmq::buffer("Error: Invalid JSON"), zmq::send_flags::none);
-//             }
-//         }
-//     }
-
-//     // auto json_msg = json::parse(text);
-
-//     // socket.send(zmq::str_buffer("Me!"), zmq::send_flags::dontwait);
-    
-//     // auto json_msg = json::parse(message.to_string());
-//     // printf("%s\n", message.);
-
-//     socket.close();
-//     context.close();
-// }
-
 #include <stdio.h>
 #include "IPC_environment.hpp"
 #include <format>
 #include <iostream>
+#include <math.h>
 
 int main() {
     IPC_Environment ipc;
     ipc.start(3563);
-
+    
     while (true) {
         std::optional<json> msg_json_opt = ipc.recvJSON();
         if (msg_json_opt) {
@@ -81,16 +17,54 @@ int main() {
             float posy = msg_json["posy"].get<float>();
             float velx = msg_json["velx"].get<float>();
             float vely = msg_json["vely"].get<float>();
+            float radians = msg_json["radians"].get<float>();
+            float radians_rate = msg_json["radians_rate"].get<float>();
 
-            std::cout << "\n(COMPUTER): Received sensor data -> " << std::format("px: {}, py: {}, vx: {}, vy: {}", posx, posy, velx, vely) << std::endl;
+            std::cout << "\n(COMPUTER): Received sensor data -> " << std::format("px: {}, py: {}, vx: {}, vy: {}, radians: {}, radians_rate: {}", posx, posy, velx, vely, radians, radians_rate) << std::endl;
 
-            float k1 = 3.0;
-            float k2 = -3.0;
+            float k1;
+            float k2;
+
+            k1 = 1.0;
+            k2 = 1.0;
+            float F_vert = k1 * posy + k2 * vely + 9.8 * 1.0;
+            float F1_vert = F_vert / 2.0;
+            if (posy < 0) {
+                F1_vert = 0;
+            }
+            float F2_vert = F1_vert; 
+
+            k1 = 0.1;
+            k2 = 0.1;
+            float target_radians = -k1 * posx - k2 * velx;
+            if (target_radians > 0.523599) {
+                target_radians = 0.523599;
+            }
+            if (target_radians < -0.523599) {
+                target_radians = -0.523599;
+            }
+
+            k1 = 0.1;
+            k2 = 0.3;
+            float torque = k1 * (target_radians - radians) - k2 * radians_rate;
+
+            float F1_horz = 0;
+            float F2_horz = 0;
+            if (torque > 0) {
+                F2_horz = abs(torque) / 1.0;
+            } else {
+                F1_horz = abs(torque) / 1.0;
+            }
 
             json response = {
-                {"Fx", k1 * posx + k2 * velx},
-                {"Fy", k1 * posy + k2 * vely},
+                {"commands", {
+                    {"F1", F1_horz + F1_vert},
+                    {"F2", F2_horz + F2_vert}
+                }}
             };
+
+            // {"Fx", k1 * posx + k2 * velx},
+            //     {"Fy", k1 * posy + k2 * vely},
 
             ipc.sendJSON(response);
         }
