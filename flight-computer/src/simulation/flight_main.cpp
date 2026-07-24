ff862c6947803d8893e1f11bad3f562cd33d9dbb
Tesseraction <timburnstech@gmail.com>
2026-07-24 00:54:00 +0000

    fixed missing LOOP_DT type

--- flight-computer/src/simulation/flight_main.cpp
+++ flight-computer/src/simulation/flight_main.cpp
@@ -3,7 +3,7 @@
 /// Treat these functions like the Arduino setup() and loop() functions. Sensor data
 /// is given to you out-of-the-box instead of through your sensor firmware.
 
-const LOOP_DT = 0.01;
+const float LOOP_DT = 0.01;
 
 ControlStep setup(json sensor_data) {
     return {.control_msg = {}, .execution_time = 0.01};
